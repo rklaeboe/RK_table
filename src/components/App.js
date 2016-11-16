6a8043d90e803407114e19d482eb0d3fe0981175
icemover <r@klaeboe.info>
2016-11-16 17:43:52 +0100

    testing unzip more

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import _ from 'lodash';
+function pushArray(arr, arr2) {
+  arr.push.apply(arr, arr2);
+}
 let Rows=[
 
     {"tellepunktID":"300231","tellepunktNavn":"AMMERUD","aar":2015,"ukenummer":"20, 25","timeperiod":"0 - 23",
@@ -9,12 +12,17 @@ let Rows=[
     "antallPasseringer":"95412","gjennomsnittligHastighet":null,"ukedager":"1, 2, 3, 4, 5, 6, 7","retning":2,
     "kompasretning":"Motgående retning","antallDager":14,"snittPasseringer":6815}];
 
-let keyArr=Object.keys(Rows[1]);
+let keyArr=[];
+keyArr.push(Object.keys(Rows[1]));
 console.log(keyArr);
 
 
 let valArr= _.map(Rows, function(obj){let tA= Object.values(obj);return tA});
-valArr=valArr.unshift(keyArr);
+console.log("valArr ",valArr);
+keyArr=keyarr.concat(valArr);
+onsole.log("keys ",keyArr);
+;
+
 let tempArr= _.unzip(valArr);
 console.log(tempArr);
 
