9f9994769c64d2ca0f4fb81def2ff007c166512c
icemover <r@klaeboe.info>
2016-11-16 01:03:53 +0100

    Initial

---
+++ src/components/App.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import Row from "./Row.js"
+
+export default class App extends Component {
+  render() {
+    return (<div><h2>Eksempel på å konvertere rader til kolonner </h2>
+      <Row />
+
+  </div>)}}
